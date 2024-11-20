bcb16bcead3926fb25994a793fe3d133dd747669
JSoi <jsoiworks@gmail.com>
2024-11-20 20:02:00 +0900

    add Safe Producer Config

--- kafka-producer-wikimedia/src/main/java/co/jsoi/demos/kafka/wikimedia/WikimediaChangesProducer.java
+++ kafka-producer-wikimedia/src/main/java/co/jsoi/demos/kafka/wikimedia/WikimediaChangesProducer.java
@@ -22,6 +22,18 @@ public class WikimediaChangesProducer {
         properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
+        // 안전한 프로듀서 설정
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all");
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE));
+
+        // 높은 처리량을 위한 프로듀서 설정
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20");
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString(32 * 1024));
+
+        // 압축 설정
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy");
+
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
         final String topic = "wikimedia.recentChange";
 
